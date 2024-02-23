42b36eee561efcfbb683f9585089ecaf55c1ae35
fedejael <federicojael.canto@gmail.com>
2024-02-23 10:23:42 -0300

    Primeros pasos

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hola Mundo!");
+/* const express = require("express"); */ //Utiliza commontJS
+
+import express from 'express' // Utiliza EM6
+
+// Crear la app
+const app = express();
+
+// Routing
+app.get("/", function(req, res) {
+    res.send("Hola Mundo en express")
+});
+
+app.get("/nosotros", function(req, res) {
+    res.send("Informacion de nosotros")
+});
+
+// Definir un puerto y arrancar el proyecto
+const port = 3000;
+app.listen(port, () => {
+    console.log("El Servidor esta funcionando en el puerto ${port}")
+});
