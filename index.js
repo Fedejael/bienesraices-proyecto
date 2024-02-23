03c6140bff445a54c8e5aed6dc6d2e14cf82562b
fedejael <federicojael.canto@gmail.com>
2024-02-23 14:11:31 -0300

    Routing y Peticiones

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 /* const express = require("express"); */ //Utiliza commontJS
 
 import express from 'express' // Utiliza EM6
+import usuarioRoutes from "./routes/usuarioRoutes.js"
 
 // Crear la app
 const app = express();
 
 // Routing
-app.get("/", function(req, res) {
-    res.send("Hola Mundo en express")
-});
+app.use("/", usuarioRoutes)
 
-app.get("/nosotros", function(req, res) {
-    res.send("Informacion de nosotros")
-});
 
 // Definir un puerto y arrancar el proyecto
 const port = 3000;
