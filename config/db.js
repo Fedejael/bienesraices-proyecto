420120eda55d047ed94fb5cc0c2009eef4db0441
fedejael <federicojael.canto@gmail.com>
2024-02-26 23:58:30 -0300

    Conectando nuestra app a una base de datos

--- config/db.js
+++ config/db.js
@@ -2,5 +2,17 @@ import Sequelize from 'sequelize'
 
 
 const db = new Sequelize("bienesraices_node_mvc", "root", "", {
-  host
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  define: {
+    timestamps: true
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorAliases: false
 })
