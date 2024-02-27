8b5c28d2250b4ad7f2e5699f36c4279d2649874b
fedejael <federicojael.canto@gmail.com>
2024-02-27 10:54:28 -0300

    Creando modelo, agregando campos y comunicando la vista routing con el controller

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -10,6 +10,10 @@ const formularioRegistro = (req, res) => {
     })
 }
 
+const registrar = (req, res) => {
+    console.log("registrando")
+}
+
 const formularioOlvidePassword = (req, res) => {
     res.render("auth/olvide-password", {
         pagina: "Repura tu acceso a Bienes Raices"
@@ -19,5 +23,6 @@ const formularioOlvidePassword = (req, res) => {
 export{
     formularioLogin,
     formularioRegistro,
+    registrar,
     formularioOlvidePassword
 }
