c0c595dfe93a9d11d38a22c8e05879db87eb0915
fedejael <federicojael.canto@gmail.com>
2024-02-28 21:06:33 -0300

    Validando formulario y agregando resto validaciones

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,6 @@
+import { check, validationResult } from "express-validator"
+import Usuario from "../models/Usuario.js"
+
 const formularioLogin = (req, res) => {
     res.render("auth/login", {
         pagina: "Iniciar Sesión"
@@ -10,8 +13,21 @@ const formularioRegistro = (req, res) => {
     })
 }
 
-const registrar = (req, res) => {
-    console.log("registrando")
+const registrar = async (req, res) => {
+    // Validación
+    await check("nombre").notEmpty().withMessage("El nombre no puede ir vacio").run(req)
+    await check("email").isEmail().withMessage("Eso no parece un email").run(req)
+    await check("password").isLength({min: 6}).withMessage("El Password debe ser de al menos 6 caracteres").run(req)
+    await check("repetir_password").equals("password").withMessage("Los Passwords no son iguales").run(req)
+
+    let resultado = validationResult(req)
+
+    // Verificar que el resultado este vacio
+
+    res.json(resultado.array())
+
+    const usuario = await Usuario.create(req.body)
+    res.json(usuario)
 }
 
 const formularioOlvidePassword = (req, res) => {
